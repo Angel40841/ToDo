faa3026925de5a27eadc9e20830e162c0ee7ac47
angel <angel.valchev1984@gmail.com>
2025-02-24 15:00:02 +0200

    added todoitem model

---
+++ springtodo/src/main/java/com/acho/springtodo/models/ToDoItem.java
@@ -0,0 +1,54 @@
+package com.acho.springtodo.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "todo-item")
+public class ToDoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private boolean complete;
+
+    @Getter
+    @Setter
+    private Instant createdDate;
+
+    @Getter
+    @Setter
+    private Instant modifiedDate;
+
+    public ToDoItem() {
+    }
+
+    public ToDoItem(String description) {
+        this.description = description;
+        this.complete = false;
+        this.createdDate = Instant.now();
+        this.modifiedDate = Instant.now();
+    }
+
+    @Override
+    public String toString() {
+        return "ToDoItem{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", complete=" + complete +
+                ", createdDate=" + createdDate +
+                ", modifiedDate=" + modifiedDate +
+                '}';
+    }
+}
